3e83e23f0dd1dbe60a78ad84ed8d9f8b86d4389f
Carlos Costa <carllos.nc@gmail.com>
2022-07-24 16:32:16 -0300

    update images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,3 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
-/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
@@ -16,11 +13,11 @@ module.exports = {
             },
 
             backgroundImage: {
-                'login': "url('https://images.unsplash.com/photo-1445452642132-b08fef5f689d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80')",
-                'register': "url('https://images.unsplash.com/photo-1580318598552-a1afea3d905b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80')",
-                'forgot-password': "url('https://images.unsplash.com/photo-1522775417749-29284fb89f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2574&q=80')",
-                'reset-password': "url('https://images.unsplash.com/photo-1550724026-f20ae6b4a592?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80')",
-                'two-factor': "url('https://images.unsplash.com/photo-1584378469704-d455e6f313fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80')"
+                'login': "url('https://res.cloudinary.com/c4co/image/upload/v1658690933/laravel-base/02_ugxdcx.jpg')",
+                'register': "url('https://res.cloudinary.com/c4co/image/upload/v1658690933/laravel-base/03_ataqag.jpg')",
+                'forgot-password': "url('https://res.cloudinary.com/c4co/image/upload/v1658690933/laravel-base/05_pmbuec.jpg')",
+                'reset-password': "url('https://res.cloudinary.com/c4co/image/upload/v1658690933/laravel-base/04_wwfena.jpg')",
+                'two-factor': "url('https://res.cloudinary.com/c4co/image/upload/v1658690933/laravel-base/01_ii8ctp.jpg')"
             }
         },
 
